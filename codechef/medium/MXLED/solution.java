6098d8b3256071657694147d13955d7939196ccb
Naman <saininaman643@gmail.com>
2026-08-10 20:46:44 +0530

    ✅ MXLED | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/MXLED/solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int p1 = 0;
+		int p2 = 0;
+		
+		int maxLead = 0;
+		int winner = 0;
+		
+		for(int i = 0; i < n; i++){
+		    int s = sc.nextInt();
+		    int t = sc.nextInt();
+		    
+		    p1+=s;
+		    p2+=t;
+		    
+		    int lead = Math.abs(p1 - p2);
+		    
+		    if(lead > maxLead){
+		        lead = maxLead;
+		        
+		        if(p1>p2){
+		            winner = 1;
+		        }
+		        else{
+		            winner = 2;
+		        }
+		    }
+		    
+		}
+		
+		System.out.println(maxLead);
+	}
+}
