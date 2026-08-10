ed5b5dec4f67bca7b59cc681db11e7c0e6eab88c
Naman <saininaman643@gmail.com>
2026-08-10 20:00:15 +0530

    ✅ DLFEE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/DLFEE/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(a<c){
+		    System.out.println(a+b);
+		}
+		else{
+		    System.out.println(a);
+		}
+	}
+}
