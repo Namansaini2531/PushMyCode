b1eea2411556499daf8a472d9108da763366c915
Naman saini <saininaman643@gmail.com>
2026-08-15 20:47:21 +0530

    ✅ Q1. Elevator Requests I | Easy | Java | Runtime: 1 ms (100.00%) | Memory: 44.8 MB | 2026-08-15

--- leetcode/easy/4384-elevator-requests-i/solution.java
+++ leetcode/easy/4384-elevator-requests-i/solution.java
@@ -2,11 +2,12 @@ class Solution {
     public int elevatorRequests(int n, int[] requests) {
         int floor = 0;
         int secs = 0;
-        for(int i = 0; i<requests.length; i++){
+
+        for(int i = 0; i < requests.length; i++){
             secs = secs + Math.abs(requests[i] - floor);
             floor = requests[i];
-            
         }
+
         return secs;
     }
 }
