532b200c90c34ef86d71007a9c1738fb07efec5b
Naman saini <saininaman643@gmail.com>
2026-08-17 20:12:45 +0530

    ✅ GROFR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/GROFR/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+	}
+}
