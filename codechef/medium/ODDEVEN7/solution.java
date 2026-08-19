cbb5537dc4c66e7c1d52019496bd8f51eb82d666
Naman saini <saininaman643@gmail.com>
2026-08-19 20:36:09 +0530

    ✅ ODDEVEN7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.java
+++ codechef/medium/ODDEVEN7/solution.java
@@ -7,6 +7,35 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int even = 0;
+		    int odd = 0;
+		    for(int i = 0; i < n; i++){
+		        int x = sc.nextInt();
+		        
+		        if(x%2==0){
+		            even++;
+		        }
+		        else{
+		            odd++;
+		        }
+		    }
+		    
+		    int ans = 0;
+		    
+		    if(even == odd){
+		        ans = n;
+		    }
+		    else{
+		        ans = 2 * Math.min(even, odd) + 1;
+		    }
+		    
+		    System.out.println(ans);
+		}
 	}
 }
