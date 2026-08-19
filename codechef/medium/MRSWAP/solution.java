50d8ef5e97a17c68a47573f8b31f0cab9c5561b6
Naman saini <saininaman643@gmail.com>
2026-08-19 20:22:58 +0530

    ✅ MRSWAP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.java
+++ codechef/medium/MRSWAP/solution.java
@@ -8,10 +8,10 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
-		int ans = 0;
 		int t = sc.nextInt();
 		
 		while(t-- > 0){
+		    int ans = 0;
 		    int n = sc.nextInt();
 		    
 		    int[] arr = new int[2 * n];
@@ -23,8 +23,7 @@ class Codechef
 	    	for(int i = 0; i < n; i++){
 		        ans = ans + Math.max(arr[i], arr[2 * n - 1 - i]);
 		    }
+		    System.out.println(ans);
 		}
-		
-		System.out.println(ans);
 	}
 }
