2872113cf556b2f85a441e6644dc65a94e564733
Naman saini <saininaman643@gmail.com>
2026-08-20 23:15:29 +0530

    ✅ Sort Colors | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.7 MB | 2026-08-20

---
+++ leetcode/medium/0075-sort-colors/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid<=high){
+            if(nums[mid] == 0){
+                int temp1 = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp1;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                int temp2 = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp2;
+                high--;
+            }
+        }
+        return;
+    }
+}
