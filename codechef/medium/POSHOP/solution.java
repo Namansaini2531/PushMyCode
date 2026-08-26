4df02c390b7af4569231739efab29749ef47cb79
Naman saini <saininaman643@gmail.com>
2026-08-26 21:31:13 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -7,6 +7,22 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    int[] arr = new int[n];
+		    
+		    int max = 0;
+		    int l = 0;
+		    int r = 0;
+		    
+		    for(int i = 0; i < n; i++){
+		        arr = sc.nextInt();
+		    }
+		}
 	}
 }
