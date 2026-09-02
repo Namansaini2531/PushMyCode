09c7de1a3d8192d4534bb508be5bec93fc5f69a3
Naman saini <saininaman643@gmail.com>
2026-09-02 20:54:11 +0530

    ✅ MISSINGNUM7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/MISSINGNUM7/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		
+		if(s >= 6 && s<=9){
+		    System.out.println(9 - s);
+		}
+	}
+}
