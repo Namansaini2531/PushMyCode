6516a1a70d847bc85ea68511fadd2c01aea6d024
Naman saini <saininaman643@gmail.com>
2026-09-02 21:24:51 +0530

    ✅ MAXSUM77 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.java
+++ codechef/medium/MAXSUM77/solution.java
@@ -7,6 +7,41 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    int k = sc.nextInt();
+		    
+		    int[] arr = new int[n];
+		    
+		    for(int i = 0; i < n; i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    
+		    int wind = n - k;
+		    
+		    int sum = 0;
+		    
+		    for(int i = 0; i < wind; i++){
+		        sum += arr[i];
+		    }
+		    
+		    int maxSum = sum;
+		    
+		    //sliding window
+		    
+		    for(int i = wind; i < n; i++){
+		        sum = sum + arr[i];
+		        sum = sum - arr[i - wind];
+		        
+		        maxSum = Math.max(sum, maxSum);
+		    }
+		    
+		    System.out.println(maxSum);
+		}
 	}
 }
