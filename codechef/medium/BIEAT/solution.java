cf4faf8674a98d4ae0a1f1f577ebcb0b837a0ecb
Naman saini <saininaman643@gmail.com>
2026-09-07 19:42:52 +0530

    ✅ BIEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/BIEAT/solution.java
+++ codechef/medium/BIEAT/solution.java
@@ -7,6 +7,20 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i : arr){
+		    arr[i] = sc.nextInt();
+		}
+		
+		int m = sc.nextInt();
+		
+		for(int i = 0; i < n; i++){
+		    arr[i] = arr[i] >> m;
+		    System.out.println(arr[i] + " ");
+		}
 	}
 }
