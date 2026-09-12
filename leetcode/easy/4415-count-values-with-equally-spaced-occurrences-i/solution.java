119e6bff84a617bcccf932e56d23444d5a8bb3df
Naman saini <saininaman643@gmail.com>
2026-09-12 21:26:02 +0530

    ✅ Q1. Count Values With Equally Spaced Occurrences I | Easy | Java | Runtime: 5 ms (100.00%) | Memory: 46.5 MB | 2026-09-12

---
+++ leetcode/easy/4415-count-values-with-equally-spaced-occurrences-i/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        int count = 0;
+
+        for (ArrayList<Integer> indices : map.values()) {
+            // Must occur exactly 3 times
+            if (indices.size() == 3) {
+                int i1 = indices.get(0);
+                int i2 = indices.get(1);
+                int i3 = indices.get(2);
+            if (i2 - i1 == i3 - i2) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
